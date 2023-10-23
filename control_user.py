f9f4cd1e97fcd448cc79b0b26c94c0f2f0aae97f
Tkadha(home) <emforhs228@naver.com>
2023-10-23 18:50:30 +0900

    user_controler 생성 아직 움직임 만들지 않음

---
+++ control_user.py
@@ -0,0 +1,58 @@
+from pico2d import *
+
+from User_char import User
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            User.handle_event(event)
+
+
+def reset_world():
+    global running
+    global team
+    global world
+    global user
+
+    running = True
+    world = []
+
+    user = User()
+    world.append(user)
+
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
