7640843706dad6e1ed861b3ce799cdff37fabae2
Tkadha(home) <emforhs228@naver.com>
2023-10-29 20:38:48 +0900

    상하이동 만듦, 대각선 이동 안됨

--- user_char.py
+++ user_char.py
@@ -38,14 +38,15 @@ def under_up(e):
 class Idle:
 
     @staticmethod
-    def enter(user,e):
+    def enter(user, e):
         user.action = 3
         user.frame = 0
-        user.speed=15
+        user.speed = 15
+        user.LR_way, user.UD_way = 0, 0
         pass
 
     @staticmethod
-    def exit(user,e):
+    def exit(user, e):
         pass
 
     @staticmethod
@@ -55,35 +56,47 @@ class Idle:
 
     @staticmethod
     def draw(user):
-        if user.dir==0:
+        if user.dir == 0:
             user.image.clip_draw(user.frame * 35, user.action * 40, 35, 40, user.x, user.y, 75, 75)
         elif user.dir == 1:
             user.image.clip_draw(35 + user.frame * 35, user.action * 40, -35, 40, user.x, user.y, 75, 75)
 
         pass
+
+
 class Run:
 
     @staticmethod
-    def enter(user,e):
+    def enter(user, e):
         user.action = 2
         user.frame = 0
-        if right_down(e) or left_up(e):
-            user.dir=0
-        elif left_down(e) or right_up(e):
-            user.dir= 1
+        if right_down(e):
+            user.LR_way = 1
+            user.dir = 0
+        elif left_down(e):
+            user.LR_way = 2
+            user.dir = 1
+        elif under_down(e):
+            user.UD_way = 1
+        elif above_down(e):
+            user.UD_way = 2
         pass
 
     @staticmethod
-    def exit(user,e):
+    def exit(user, e):
         pass
 
     @staticmethod
     def do(user):
         user.frame = (user.frame + 1) % 4
-        if user.dir==0:
+        if user.LR_way == 1:
             user.x += user.speed
-        elif user.dir==1:
+        elif user.LR_way == 2:
             user.x -= user.speed
+        if user.UD_way == 1:
+            user.y -= user.speed - 5
+        elif user.UD_way == 2:
+            user.y += user.speed + 5
         if user.speed <= 30:
             user.speed += 5
         pass
@@ -102,12 +115,14 @@ class StateMachine:
         self.user = user
         self.cur_state = Idle
         self.transitions = {
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, above_down: Run, above_up: Run, under_down: Run, under_up: Run},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, above_down: Idle, above_up: Idle, under_down: Idle, under_up: Idle}
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, above_down: Run, above_up: Run,
+                   under_down: Run, under_up: Run},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, above_down: Idle, above_up: Idle,
+                  under_down: Idle, under_up: Idle}
         }
 
     def start(self):
-        self.cur_state.enter(self.user, ('START',0))
+        self.cur_state.enter(self.user, ('START', 0))
 
     def update(self):
         self.cur_state.do(self.user)
@@ -134,6 +149,8 @@ class User:
         self.dir = 0
         self.action = 3
         self.speed = 15
+        self.LR_way = 1
+        self.UD_way = 1
         if User.image == None:
             User.image = load_image('red_hockey.png')
         self.state_machine = StateMachine(self)
