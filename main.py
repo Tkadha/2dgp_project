ea6da0b0728c0fccb51079f87e395d04352a0485
Tkadha(home) <emforhs228@naver.com>
2023-11-10 18:50:08 +0900

    import game_framework

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import pico2d
 import play_mode
-
+import game_framework
 canvas_width=1200
 canvas_height=800
 
 pico2d.open_canvas(canvas_width,canvas_height)
-play_mode.init()
-# game loop
-while play_mode.running:
-    play_mode.handle_events()
-    play_mode.update()
-    play_mode.draw()
-    pico2d.delay(0.1)
-
-play_mode.finish()
-# finalization code
+game_framework.run(play_mode)
 pico2d.close_canvas()
