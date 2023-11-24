a4eb63ed1b43055097077f06255b80f7e61e1087
Tkadha(home) <emforhs228@naver.com>
2023-11-24 19:34:44 +0900

    title_mode.py 완료

---
+++ title_mode.py
@@ -0,0 +1,32 @@
+from pico2d import *
+
+import game_framework
+import play_mode
+
+def init():
+    global image
+    image = load_image('./resource/title.png')
+
+
+def finish():
+    global image
+    del image
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.change_mode(play_mode)
+
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    image.draw(600, 400)
+    update_canvas()
