8454720954eeb18946105fbcc0b622002570accc
Tkadha(home) <emforhs228@naver.com>
2023-11-27 18:29:07 +0900

    점수판 추가

--- field.py
+++ field.py
@@ -1,12 +1,17 @@
-from pico2d import load_image, draw_rectangle
+from pico2d import load_image, draw_rectangle, load_font
 
 
 class Field:
     def __init__(self):
         self.image = load_image('./resource/field.png')
+        self.font = load_font('ENCR10B.TTF', 40)
+        self.left_score=0
+        self.right_score=0
 
     def draw(self):
         self.image.clip_draw(0, 0, 512, 240, 1200 // 2, 800 // 2, 1200, 800)
+        self.font.draw(430, 730, f'{self.left_score}', (255, 255, 255))
+        self.font.draw(730, 730, f'{self.right_score}', (255, 255, 255))
         draw_rectangle(*self.get_bb())
 
     def update(self):
