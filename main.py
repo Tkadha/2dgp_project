fc5870c3a89718bcd73244cbde85f8c0152f07f6
Tkadha(home) <emforhs228@naver.com>
2023-12-04 00:09:04 +0900

    title 부터 시작

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pico2d
 import play_mode
+import title_mode
 import game_framework
 canvas_width=1200
 canvas_height=800
 
 pico2d.open_canvas(canvas_width,canvas_height)
-game_framework.run(play_mode)
+game_framework.run(title_mode)
 pico2d.close_canvas()
