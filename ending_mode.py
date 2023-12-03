f0dd68271f227c42d02450ad509d94ae3f126d5b
Tkadha(home) <emforhs228@naver.com>
2023-12-04 01:24:40 +0900

    끝났을 때 최종 스코어가 나옴

--- ending_mode.py
+++ ending_mode.py
@@ -7,11 +7,21 @@ import title_mode
 def init():
     global background
     global bgm
+    global font
+    font = load_font('ENCR10B.TTF', 200)
     background = load_image('./resource/ending.png')
     bgm = load_music('./sound/ending.mp3')
     bgm.set_volume(32)
     bgm.play()
 
+
+def score(left, right):
+    global left_score
+    global right_score
+    left_score = left
+    right_score = right
+
+def vic_char(image):
 
 def finish():
     global background
@@ -26,12 +36,17 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            bgm.stop()
             game_framework.change_mode(title_mode)
+
 
 def update():
     pass
 
+
 def draw():
     clear_canvas()
     background.draw(600, 400)
+    font.draw(200, 400, f'{left_score}', (255, 255, 255))
+    font.draw(850, 400, f'{right_score}', (255, 255, 255))
     update_canvas()
